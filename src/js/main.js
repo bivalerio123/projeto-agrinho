5cd2fbf6aea11bf7f1c0c2ad81c00aa50570dbf7
bivalerio123 <105952990+bivalerio123@users.noreply.github.com>
2024-07-04 20:25:29 -0300

    ss

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $('.btn-celular').on('click', function () {
-        $('navcelular').toggleClass('ativo');
-    $('.btn-celular').find('i').toggleClass('fa-x');
-  
-    });
-} );
+        $('.navcelular').toggleClass('ativo');
+        $('.btn-celular').find('i').toggleClass('fa-x');
+    })
+    
+})
